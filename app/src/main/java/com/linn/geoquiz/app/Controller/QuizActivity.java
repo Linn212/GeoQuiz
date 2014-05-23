629ad12d6d199ca50151732aa15ea982b088248e
Linn212 <nonagon.lin@gmail.com>
2014-05-23 17:25:07 +0800

    End of Chap. 4

--- app/src/main/java/com/linn/geoquiz/app/Controller/QuizActivity.java
+++ app/src/main/java/com/linn/geoquiz/app/Controller/QuizActivity.java
@@ -2,6 +2,7 @@ package com.linn.geoquiz.app.Controller;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,27 +10,65 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.linn.geoquiz.app.Model.TrueFalse;
 import com.linn.geoquiz.app.R;
 
 
 public class QuizActivity extends ActionBarActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+        new TrueFalse(R.string.question_africa, false),
+        new TrueFalse(R.string.question_americas, true),
+        new TrueFalse(R.string.question_asia, true),
+        new TrueFalse(R.string.question_mideast, false),
+        new TrueFalse(R.string.question_oceans, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestion(){
+        Log.d(TAG, "Updating question text for question #"+ mCurrentIndex, new Exception());
+        int question = mQuestionBank[mCurrentIndex].getmQuestion();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismTrueQuestion();
+        int messageResId=0;
+
+        if (userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        }else{
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId,Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        //setContentView is called when screen is rotated.
         setContentView(R.layout.activity_quiz);
+
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestion();
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -37,12 +76,69 @@ public class QuizActivity extends ActionBarActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length) % mQuestionBank.length;
+                Log.d(TAG, "mCurrnetIndex is " + mCurrentIndex);
+                updateQuestion();
+            }
+        });
+
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCurrentIndex=(mCurrentIndex+1) % mQuestionBank.length;
+                Log.d(TAG, "mCurrnetIndex is " + mCurrentIndex);
+                updateQuestion();
+            }
+        });
+
+        if (savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        updateQuestion();
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
